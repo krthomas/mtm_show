cca4529e85212316237c104b1758fd452a1bb604
Kathryn Thomas <kathryn.thomas@sv.cmu.edu>
2011-09-25 16:28:17 -0400

    Backup before adding rspec files

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,7 @@
 class Episode < ActiveRecord::Base
+  attr_accessible :season, :episode_number, :title, :protagonist, :did_mary_date, :dates_name, :dates_hair_color
+
+  validates :season, :episode_number, :title => true
+
   has_many :reviews
 end
